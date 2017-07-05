06be59e8a9822af81dc75c8c79c3727d5964f442
宇中 <416862549qq.com@yuzhongdeiMac.local>
2017-07-05 11:10:08 +0800

    68

---
+++ LRCAReplicatorLayerDemo/LRCAReplicatorLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRCAReplicatorLayerDemo
+//
+//  Created by 宇中 on 2017/7/5.
+//  Copyright © 2017年 广州宇中网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

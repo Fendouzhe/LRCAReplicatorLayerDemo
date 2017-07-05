96169f27c38eaadb923921e0f532db04c211cf1a
宇中 <416862549qq.com@yuzhongdeiMac.local>
2017-07-05 12:43:27 +0800

    68

--- LRCAReplicatorLayerDemo/ViewController.m
+++ LRCAReplicatorLayerDemo/ViewController.m
@@ -23,6 +23,68 @@
     
 }
 
+- (void)musicAnimation{
+    
+    CAReplicatorLayer *replicatorLayer = [CAReplicatorLayer layer];
+    replicatorLayer.bounds = CGRectMake(0, 0, 200, 100);
+    replicatorLayer.position = CGPointMake(self.view.center.x, 100);
+    replicatorLayer.backgroundColor = [UIColor grayColor].CGColor;
+    [self.view.layer addSublayer:replicatorLayer];
+    
+    CALayer *subLayer = [CALayer layer];
+    [replicatorLayer addSublayer:subLayer];
+    subLayer.backgroundColor = [UIColor redColor].CGColor;
+    subLayer.bounds = CGRectMake(0, 0, 8, 40);
+    subLayer.position = CGPointMake(10, 115);
+    subLayer.cornerRadius = 2;
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    animation.fromValue = @(subLayer.position.y);
+    animation.toValue = @(subLayer.position.y-30);
+    animation.repeatCount = INFINITY;
+    animation.duration = 0.5;
+    animation.autoreverses = YES;
+    [subLayer addAnimation:animation forKey:nil];
+    
+    replicatorLayer.instanceCount = 10;
+    replicatorLayer.instanceDelay = 0.33;
+    replicatorLayer.instanceTransform = CATransform3DMakeTranslation(20, 0, 0);
+    replicatorLayer.masksToBounds = YES;//超出部分不显示
+}
+
+- (void)activityIndicatorAnimation{
+    
+    CAReplicatorLayer *replicator = [CAReplicatorLayer layer];
+    replicator.backgroundColor = [UIColor grayColor].CGColor;
+    [self.view.layer addSublayer:replicator];
+    replicator.bounds = CGRectMake(0, 0, 200, 200);
+    replicator.position = CGPointMake(self.view.center.x, self.view.center.y-80);
+    
+    CALayer *subLayer = [CALayer layer];
+    [replicator addSublayer:subLayer];
+    subLayer.bounds = CGRectMake(0, 0, 14, 14);
+    subLayer.position = CGPointMake(replicator.bounds.size.width*0.5, 30);
+    subLayer.backgroundColor = [UIColor redColor].CGColor;
+    subLayer.cornerRadius = 8;
+    
+    CFTimeInterval duration = 1.5;
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    animation.fromValue = @(1.0);
+    animation.toValue = @(0.1);
+    animation.duration = duration;
+    animation.repeatCount = INFINITY;
+    //animation.autoreverses = YES;
+    [subLayer addAnimation:animation forKey:@"transform.scale"];
+    
+    replicator.instanceCount = 15;
+    replicator.instanceDelay = duration/replicator.instanceCount;
+    CGFloat angle = M_PI*2/replicator.instanceCount;
+    replicator.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1.0);
+    
+}
+
+
+
 - (void)followAnimation
 {
     CAReplicatorLayer *replicatorLayer = [CAReplicatorLayer new];
@@ -79,65 +141,7 @@
 }
 
 
-- (void)activityIndicatorAnimation{
-    
-    CAReplicatorLayer *replicator = [CAReplicatorLayer layer];
-    replicator.backgroundColor = [UIColor grayColor].CGColor;
-    [self.view.layer addSublayer:replicator];
-    replicator.bounds = CGRectMake(0, 0, 200, 200);
-    replicator.position = CGPointMake(self.view.center.x, self.view.center.y-80);
-    
-    CALayer *subLayer = [CALayer layer];
-    [replicator addSublayer:subLayer];
-    subLayer.bounds = CGRectMake(0, 0, 14, 14);
-    subLayer.position = CGPointMake(replicator.bounds.size.width*0.5, 30);
-    subLayer.backgroundColor = [UIColor redColor].CGColor;
-    subLayer.cornerRadius = 8;
-    
-    CFTimeInterval duration = 1.5;
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-    animation.fromValue = @(1.0);
-    animation.toValue = @(0.1);
-    animation.duration = duration;
-    animation.repeatCount = INFINITY;
-    //animation.autoreverses = YES;
-    [subLayer addAnimation:animation forKey:@"transform.scale"];
-    
-    replicator.instanceCount = 15;
-    replicator.instanceDelay = duration/replicator.instanceCount;
-    CGFloat angle = M_PI*2/replicator.instanceCount;
-    replicator.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1.0);
-    
-}
 
-- (void)musicAnimation{
-
-    CAReplicatorLayer *replicatorLayer = [CAReplicatorLayer layer];
-    replicatorLayer.bounds = CGRectMake(0, 0, 200, 100);
-    replicatorLayer.position = CGPointMake(self.view.center.x, 100);
-    replicatorLayer.backgroundColor = [UIColor grayColor].CGColor;
-    [self.view.layer addSublayer:replicatorLayer];
-    
-    CALayer *subLayer = [CALayer layer];
-    [replicatorLayer addSublayer:subLayer];
-    subLayer.backgroundColor = [UIColor redColor].CGColor;
-    subLayer.bounds = CGRectMake(0, 0, 8, 40);
-    subLayer.position = CGPointMake(10, 115);
-    subLayer.cornerRadius = 2;
-    
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-    animation.fromValue = @(subLayer.position.y);
-    animation.toValue = @(subLayer.position.y-30);
-    animation.repeatCount = INFINITY;
-    animation.duration = 0.5;
-    animation.autoreverses = YES;
-    [subLayer addAnimation:animation forKey:nil];
-    
-    replicatorLayer.instanceCount = 10;
-    replicatorLayer.instanceDelay = 0.33;
-    replicatorLayer.instanceTransform = CATransform3DMakeTranslation(20, 0, 0);
-    replicatorLayer.masksToBounds = YES;//超出部分不显示
-}
 
 
 @end
